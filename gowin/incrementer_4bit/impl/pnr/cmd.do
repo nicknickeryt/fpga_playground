d4d639a66cac50e9dc1493530256e5a0264227ee
Nick Nicker <nick4nicker@gmail.com>
2024-11-19 18:39:32 +0100

    Add Tang Nano 4 bit incrementer implementation

---
+++ gowin/incrementer_4bit/impl/pnr/cmd.do
@@ -0,0 +1,14 @@
+-d /home/nick/Dokumenty/AGH/CUE/Verilog/github_repo/gowin/incrementer_4bit/impl/gwsynthesis/incrementer_4bit.vg
+-p GW1NSR-4C-QFN48P-7
+-pn GW1NSR-LV4CQN48PC7/I6
+-cst /home/nick/Dokumenty/AGH/CUE/Verilog/github_repo/gowin/incrementer_4bit/src/incrementer_4bit.cst
+-cfg /home/nick/Dokumenty/AGH/CUE/Verilog/github_repo/gowin/incrementer_4bit/impl/pnr/device.cfg
+-bit
+-tr
+-ph
+-timing
+-cst_error
+-convert_sdp32_36_to_sdp16_18
+-correct_hold 1
+-route_maxfan 23
+-global_freq 50.000
---
+++ gowin/incrementer_4bit/src/incrementor_4bit.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/19/2024 02:29:35 PM
+// Design Name: 
+// Module Name: incrementor_4bit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module incrementor_4bit(
+    input [3:0] x,
+    output [3:0] y
+    );
+    
+    wire [4:0] a;
+  
+    assign a[0] = 1;
+    
+    half_adder halfAdder1 (a[0], ~x[0], y[0], a[1]);
+    half_adder halfAdder2 (a[1], ~x[1], y[1], a[2]);
+    half_adder halfAdder3 (a[2], ~x[2], y[2], a[3]);
+    half_adder halfAdder4 (a[3], ~x[3], y[3], a[4]);
+    
+    
+endmodule
