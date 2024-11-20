c7fd5ccebb2951d45fa5a6cdecbc0b90b834df30
Nick Nicker <nick4nicker@gmail.com>
2024-11-21 00:04:46 +0100

    gowin: 7seg_test: add debug LED output

--- gowin/7seg_test/src/seg7.v
+++ gowin/7seg_test/src/seg7.v
@@ -1,10 +1,14 @@
 `timescale 1ns / 1ps
 
 module seg7(
-    input  [3:0] x,
-    output a, b, c, d, e, f, g, com);
+    input [3:0] x,
+    output a, b, c, d, e, f, g, com,
+    output [3:0] leds);
+
+assign leds = x;
 
 assign com = 0;
+
 assign a = ( (~x[2]) & (~x[0]) )  | x[1] | ( x[2] & x[0] ) | x[3];
 assign b = (~x[2]) | ( (~x[1]) & (~x[0] ) ) | (x[1] & x[0]);
 assign c = (~x[1]) | x[0] | x[2];
